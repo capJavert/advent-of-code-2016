a57c5e8b984f24d4d4b15cf6d63a5142115b09ed
capJavert <ante.baric3@gmail.com>
2016-12-06 23:21:34 +0100

    6-day part 1

---
+++ 6-day.py
@@ -0,0 +1,39 @@
+import requests
+
+
+def get_and_prepare_data_string():
+    """
+    fetch data for my user from pastebin triangle string was just to big to insert inside this code :)
+    """
+    request = requests.get("http://pastebin.com/raw/9FAXzEqD")
+    request.encoding = 'ISO-8859-1'
+
+    return request.text.splitlines()
+
+
+def main():
+    data_strings = get_and_prepare_data_string()
+
+    message = ""
+    message_length = len(data_strings[0])
+    letters = "abcdefghijklmnopqrstuvwxyz"
+    position_counts = []
+
+    for i in range(0, message_length):
+        letter_count = {}
+
+        for l in letters:
+            letter_count[l] = 0
+
+        position_counts.append(letter_count)
+
+    for string in data_strings:
+        for (index, letter) in enumerate(string):
+            position_counts[index][letter] += 1
+
+    for count in position_counts:
+        message += max(count, key=lambda p: count[p])
+
+    print(message)
+
+main()
