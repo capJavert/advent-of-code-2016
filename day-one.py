4efa05599356dd99b87ccd9a1e2dbea36ff2404e
capJavert <ante.baric3@gmail.com>
2016-12-06 12:40:04 +0100

    day-one part 1

---
+++ day-one.py
@@ -0,0 +1,33 @@
+
+def get_vectors_from_string(data_string):
+    return data_string.split(", ")
+
+
+def calc_distance(vectors):
+    orientation = 0
+    distance = 0
+
+    for vector in vectors:
+        if vector[0] == "L":
+            orientation -= 90
+        else:
+            orientation += 90
+
+        orientation %= 360
+
+        if orientation == 0 or orientation == 90:
+            distance += int(vector[1:4])
+        else:
+            distance -= int(vector[1:4])
+
+    return distance
+
+
+def main():
+    data_string = "L5, R1, L5, L1, R5, R1, R1, L4, L1, L3, R2, R4, L4, L1, L1, R2, R4, R3, L1, R4, L4, L5, L4, R4, L5, R1, R5, L2, R1, R3, L2, L4, L4, R1, L192, R5, R1, R4, L5, L4, R5, L1, L1, R48, R5, R5, L2, R4, R4, R1, R3, L1, L4, L5, R1, L4, L2, L5, R5, L2, R74, R4, L1, R188, R5, L4, L2, R5, R2, L4, R4, R3, R3, R2, R1, L3, L2, L5, L5, L2, L1, R1, R5, R4, L3, R5, L1, L3, R4, L1, L3, L2, R1, R3, R2, R5, L3, L1, L1, R5, L4, L5, R5, R2, L5, R2, L1, L5, L3, L5, L5, L1, R1, L4, L3, L1, R2, R5, L1, L3, R4, R5, L4, L1, R5, L1, R5, R5, R5, R2, R1, R2, L5, L5, L5, R4, L5, L4, L4, R5, L2, R1, R5, L1, L5, R4, L3, R4, L2, R3, R3, R3, L2, L2, L2, L1, L4, R3, L4, L2, R2, R5, L1, R2"
+
+    vectors = get_vectors_from_string(data_string)
+
+    print(calc_distance(vectors))
+
+main()
