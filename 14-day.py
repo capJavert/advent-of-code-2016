fb3d1b768ab034e02f77459d83ac8c4a470701b1
capJavert <ante.baric3@gmail.com>
2016-12-14 14:00:39 +0100

    finished 14-day

--- 14-day.py
+++ 14-day.py
@@ -1,4 +1,7 @@
 from hashlib import md5
+import time
+
+hashes = {}
 
 
 def print_maze(maze):
@@ -26,6 +29,23 @@ def find_first_string(string):
         return -1
 
 
+def stretch_hash(hash_string):
+    source_hash = hash_string
+
+    if source_hash not in hashes:
+        for j in range(0, 2016):
+            m = md5()
+            string = hash_string
+            m.update(string.encode())
+            hash_string = m.hexdigest()
+
+        hashes[source_hash] = hash_string
+    else:
+        hash_string = hashes[source_hash]
+
+    return hash_string
+
+
 def main():
     salt = "qzyelonm"
     keys = []
@@ -38,9 +58,11 @@ def main():
             break
 
         m = md5()
-        string = salt+str(index)
+        string = salt + str(index)
         m.update(string.encode())
         hash_string = m.hexdigest()
+
+        hash_string = stretch_hash(hash_string)
 
         char = find_first_string(hash_string)
         if char != -1:
@@ -50,9 +72,13 @@ def main():
                 m.update(string.encode())
                 check_string = m.hexdigest()
 
+                check_string = stretch_hash(check_string)
+
                 if char+char+char+char+char in check_string:
                     keys.append(hash_string)
                     print(index, hash_string)
                     break
 
+start_time = time.time()
 main()
+print("--- %s seconds ---" % (time.time() - start_time))
