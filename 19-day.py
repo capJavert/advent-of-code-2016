ee221f6ea181e5bb9079ce99fa9488fb98ad5dfc
capJavert <ante.baric3@gmail.com>
2016-12-20 01:57:30 +0100

    finished 19-day

--- 19-day.py
+++ 19-day.py
@@ -1,10 +1,15 @@
 import time
 
-n = 3014387
+bn = 3014387
 
 
 def slow_main():
-    global n
+    """
+    part 1 implementation
+    part 2 would be more slow and more ugly :) check main_slow2() function for this
+    :return:
+    """
+    n = 3014387
     elf = 0
     elves = [1] * n
     lap = False
@@ -43,33 +48,46 @@ def slow_main():
     print(elf+1)
 
 
-def main():
-    global n
+def slow_main2(no):
+    n = no
     elves = []
 
     for i in range(0, n):
-        elves.append(i+1)
+        elves.append(i + 1)
 
+    i = int(n / 2)
     while len(elves) != 1:
-        temp_elves = []
-        for i in range(0, n, 2):
-            temp_elves.append(elves[i])
+        if n == 1:
+            break
 
-        if n % 2 != 0:
-            temp_elves.pop(0)
-
-        elves = temp_elves
+        elves.pop(i)
         n = len(elves)
+
+        if n % 2 == 0:
+            i += 1
+
+        i %= n
 
     print(elves[0])
 
 
+def main(no):
+    w = 1
+
+    for i in range(1, no):
+        w = w % i + 1
+
+        if w > (i + 1) / 2:
+            w += 1
+
+    print(w)
+
 start_time = time.time()
-main()
+main(bn)
 print("--- %s seconds ---" % (time.time() - start_time))
 
 """
-SLOW IMPLEMENTATION RUNS FOR OVER 1h 30m on my 2.9GHz i5
+SLOW IMPLEMENTATION, PART 1 RUNS FOR OVER 1h 30m on my 2.9GHz i5
 ONLY HERE TO SHOW HOW MY ALGORITHM SOLUTION IS (PAPER AND PEN BEFORE MAC AND KEYBOARD!!)
 start_time = time.time()
 slow_main()
